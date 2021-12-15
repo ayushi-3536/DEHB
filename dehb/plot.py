d981f2140b42f26b92a387cc817a4cf98227abb4
ayushi-3536 <ayushi.sharma.3536@gmail.com>
2021-12-15 15:47:43 +0100

    "minimization bug"

--- dehb/plot.py
+++ dehb/plot.py
@@ -1,11 +1,12 @@
 import json
 import os
 import glob
+import argparse
 
 def load_json_file(path,spath,idx):
     # Opening JSON file
     f = open(path)
-    fs = open(spath+'\\'+'_'+str(idx)+'.txt','w')
+    fs = open(spath+'/'+'_'+str(idx)+'.txt','w')
     data = (f.read().split("\n"))
     print(len(data))
     try:
@@ -18,12 +19,15 @@ def load_json_file(path,spath,idx):
     f.close()
     fs.close()
 
-
-path = 'C:\\Users\\ayush\\PycharmProjects\\DEHB\\DEHB\\'
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', default='bukin',
+                    type=str,  help='path for json file to read')
+args= parser.parse_args()
+path = os.getcwd()+'/'+args.path
 extension = 'json'
 os.chdir(path)
 result = glob.glob('*.{}'.format(extension))
 print(result)
 for idx,f in enumerate(result):
-        load_json_file(path + '\\' + f,path,idx)
+        load_json_file(path + '/' + f,path,idx)
 
